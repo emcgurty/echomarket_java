22b7e1ca0337d6f514ca91e63f1e96d55ef28c1f
Liz McGurty <emcgurty2@gmail.com>
2016-10-02 15:28:10 -0400

    Work in progress..Horrible day... I realize that Netbeans is very flawed in debugging, I have learned to end Netbeans and restart to see desirable results.  Made very minor changes and suddenly huge errors between Netbeans, JQuery and GitHub, see previous comments. Upgraded to more current version of JQuery, and implemented Javascript as only necessary...  Seems okay but not thoroughly tested.

---
+++ web/resources/javascript/registration.js
@@ -0,0 +1,46 @@
+$.noConflict();
+jQuery(document).ready(function ($) {
+    alert("In registration");
+
+    size_of_menu();
+    $(window).resize(function () {
+        size_of_menu();
+    });
+    $('select').on('change', function () {
+        alert("select change");
+    });
+    $('input').on('change', function () {
+        alert("input change");
+        if (this.id.includes('registrationType')) {
+            alert("Okay");
+            var y_n = $(this).val();
+            if (y_n == 5){
+
+                $("div#communityNameDiv").css("display", "block");
+                $("div#onePersonReg").css("display", "none");
+            } else {
+                $("div#communityNameDiv").css("display", "none");
+                $("div#onePersonReg").css("display", "block");
+            }
+        }
+    });
+    
+    function size_of_menu() {
+
+    }
+
+});
+
+
+
+function getChildID(parentID) {
+    var child_id = null;
+    child_id = parentID.replace('rb:', '');
+    var lastColon = child_id.lastIndexOf(":");
+    if (lastColon > 0) {
+        var lengthOfSelectId = child_id.length;
+        child_id = child_id.substring(lastColon + 1, lengthOfSelectId);
+    }
+    return child_id;
+}
+
