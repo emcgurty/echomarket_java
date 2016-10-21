77f6d7b4f60e42c27179b02a492d02ef45936ce9
Liz McGurty <emcgurty2@gmail.com>
2016-10-21 13:58:49 -0400

    testing GitHub and Netbeans

--- src/java/echomarket/web/managedbeans/CommunityMembersBean.java
+++ src/java/echomarket/web/managedbeans/CommunityMembersBean.java
@@ -8,7 +8,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-//import javax.enterprise.context.RequestScoped;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
