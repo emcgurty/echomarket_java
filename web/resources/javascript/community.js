35c1e3daad904eb2429415adb8a804e3ac65560b
Liz McGurty <emcgurty2@gmail.com>
2016-10-23 08:54:45 -0400

    Add to research best regex for email validation

--- web/resources/javascript/community.js
+++ web/resources/javascript/community.js
@@ -5,16 +5,34 @@ $(document).ready(function () {
         var current_value = null;
         $("span.error-message").css("visibility", "hidden");
         $("span.error-message").text("");
-        var current_id = null;
-       if (this.id.includes('howManyRecords')) {
+        var current_id = this.id;
+//        alert(current_id);
+        if (this.id.includes('howManyRecords')) {
             current_id = this.id;
             current_value = $(this).val();
-            
-            if ( (current_value < 1) || (current_value > 25)  || (isNaN(current_value))) {
-                $("span#howManyRecords"  + ".error-message").css("visibility", "visible");
-                $("span#howManyRecords"  + ".error-message").text("Please enter a numeric value betweem 1 and 25.");
+
+            if ((current_value < 1) || (current_value > 25) || (isNaN(current_value))) {
+                $("span#howManyRecords" + ".error-message").css("visibility", "visible");
+                $("span#howManyRecords" + ".error-message").text("Please enter a numeric value betweem 1 and 25.");
             }
-                
+
+
+        } else if (this.id.includes('email')) {
+            current_id = this.id;
+            var intValue = parseInt(current_id.match(/[0-9]+/)[0], 10);
+            current_value = $(this).val();
+            
+            if (!(isEmail(current_value))) {
+                $("span#email" + intValue + ".error-message").css("visibility", "visible");
+                $("span#email" + + intValue + ".error-message").text("Please enter a valid email address.");
+            }
+
+
         }
     });
+
+    function isEmail(email) {
+        var regex = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+        return regex.test(email);
+    }
 });
