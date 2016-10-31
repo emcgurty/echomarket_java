fc403353a9fd451842f23ee1c4639a585d2ca16f
Liz McGurty <emcgurty2@gmail.com>
2016-10-31 05:35:49 -0400

    Added method to retreive user IP address...not tested

--- src/java/echomarket/web/managedbeans/AbstractBean.java
+++ src/java/echomarket/web/managedbeans/AbstractBean.java
@@ -17,6 +17,7 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.persistence.Id;
+import javax.servlet.http.HttpServletRequest;
 import org.hibernate.Session;
 
 /**
@@ -142,6 +143,44 @@ public abstract class AbstractBean implements Serializable {
                         clientId,
                         new FacesMessage(text));
     }
+    
+    public static String getClientIpAddr(HttpServletRequest request) {  
+    String ip = request.getHeader("X-Forwarded-For");  
+    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+        ip = request.getHeader("Proxy-Client-IP");  
+    }  
+    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+        ip = request.getHeader("WL-Proxy-Client-IP");  
+    }  
+    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+        ip = request.getHeader("HTTP_X_FORWARDED_FOR");  
+    }  
+    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+        ip = request.getHeader("HTTP_X_FORWARDED");  
+    }  
+    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+        ip = request.getHeader("HTTP_X_CLUSTER_CLIENT_IP");  
+    }  
+    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+        ip = request.getHeader("HTTP_CLIENT_IP");  
+    }  
+    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+        ip = request.getHeader("HTTP_FORWARDED_FOR");  
+    }  
+    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+        ip = request.getHeader("HTTP_FORWARDED");  
+    }  
+    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+        ip = request.getHeader("HTTP_VIA");  
+    }  
+    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+        ip = request.getHeader("REMOTE_ADDR");  
+    }  
+    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
+        ip = request.getRemoteAddr();  
+    }  
+    return ip;  
+}
 
     
 }
