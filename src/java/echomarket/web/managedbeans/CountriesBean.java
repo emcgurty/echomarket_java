f2d47b3199ab3ed0f4103d13b49f4a447fcc6d1b
Liz McGurty <emcgurty2@gmail.com>
2016-11-02 11:24:16 -0400

    Added method to get one Country

--- src/java/echomarket/web/managedbeans/CountriesBean.java
+++ src/java/echomarket/web/managedbeans/CountriesBean.java
@@ -1,6 +1,5 @@
 package echomarket.web.managedbeans;
 
-
 import echomarket.hibernate.Countries;
 import java.io.Serializable;
 import java.util.List;
@@ -9,54 +8,70 @@ import javax.faces.bean.ManagedBean;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
-/**
- *
- * @author emm
- */
 @ManagedBean(name = "country")
 @RequestScoped
 public class CountriesBean extends AbstractBean implements Serializable {
-
-    /**
-     * Creates a new instance of PurposeBean
-     */
     
     private String countryId;
     private String countryName;
 
-
     public CountriesBean() {
     }
 
-    
+ public String getOneCountry(String one_country) {
+
+        List result = null;
+        Session session = hib_session();
+        String returnCountry = null;
+
+        try {
+            result = session.createQuery("from Countries WHERE counry_id = :one_country")
+                    .setParameter("one_country", one_country)
+                    .list();
+        } catch (Exception e) {
+            System.out.println("Error at line 28 in Country Bean");
+            e.printStackTrace();
+
+        }
+        if (result.size() > 0) {
+            Countries returnedCountryName = (Countries) result.get(0);
+            returnCountry = returnedCountryName.getCountryName();
+            returnedCountryName= null;
+        } else {
+            returnCountry = "Country Not Found";
+        }
+        
+        session = null;
+        result = null;
+        return returnCountry;
+    }    
+ 
     public Countries[] buildCountries() {
         Countries[] purposeArray = null;
-        List purpose_list = null;
-        purpose_list = purpose_list();
-        int size_of_list = purpose_list.size();
+        List country_list = null;
+        country_list = country_list();
+        int size_of_list = country_list.size();
         purposeArray = new Countries[size_of_list];
         for (int i = 0; i < size_of_list; i++) {
-            Countries to_Array = (Countries) purpose_list.get(i);
+            Countries to_Array = (Countries) country_list.get(i);
             purposeArray[i] = new Countries(to_Array.getCountryId(), to_Array.getCountryName());
         }
         return purposeArray;
     }
 
-    private List purpose_list() {
+    private List country_list() {
 
         List result = null;
         Session session = hib_session();
-        Transaction tx = session.beginTransaction();
-
+        
         try {
             result = session.createQuery("from Countries ORDER BY id").list();
         } catch (Exception e) {
-            System.out.println("Error at line 52 in Country Bean");
+            System.out.println("Error at line 42 in Country Bean");
             e.printStackTrace();
-
         }
-        tx.commit();
-
+        
+        session = null;
         return result;
     }
 
