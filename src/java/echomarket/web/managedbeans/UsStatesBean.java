1bfb172d991597662b3a51b285d6a1595126e445
Liz McGurty <emcgurty2@gmail.com>
2016-11-02 13:41:22 -0400

    Nested Transaction error!

--- src/java/echomarket/web/managedbeans/UsStatesBean.java
+++ src/java/echomarket/web/managedbeans/UsStatesBean.java
@@ -29,14 +29,16 @@ public class UsStatesBean extends AbstractBean implements Serializable {
 
         List result = null;
         Session session = hib_session();
+        Transaction tx = session.beginTransaction();
         String returnState = null;
 
         try {
             result = session.createQuery("from UsStates WHERE id = :one_state")
                     .setParameter("one_state", one_state)
                     .list();
+            tx.commit();
         } catch (Exception e) {
-            System.out.println("Error at line 52 in US Bean");
+            System.out.println("Error at line 36 in US Bean");
             e.printStackTrace();
 
         }
@@ -48,12 +50,14 @@ public class UsStatesBean extends AbstractBean implements Serializable {
             returnState = "State not found";
         }
         
+        tx = null;
         session = null;
         result = null;
         return returnState;
     }
 
     public UsStates[] buildUsStates() {
+    
         UsStates[] purposeArray = null;
         List us_list = null;
         us_list = us_list();
@@ -70,16 +74,24 @@ public class UsStatesBean extends AbstractBean implements Serializable {
     private List us_list() {
 
         List result = null;
-        Session session = hib_session();
+        Session session = null;
+        Transaction tx = null;
+        
+        try {
+            session = hib_session();
+            tx = session.beginTransaction();
+        } catch(Exception ex){}
 
         try {
             result = session.createQuery("from UsStates ORDER BY id").list();
+            tx.commit();
         } catch (Exception e) {
             System.out.println("Error at line 52 in US Bean");
             e.printStackTrace();
 
         }
-
+        session = null;
+        tx = null;
         session = null;
 
         return result;
