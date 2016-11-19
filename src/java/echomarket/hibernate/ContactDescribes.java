e08043dba71d0cb2458a00302e14a14d0b4e19ae
Liz McGurty <emcgurty2@gmail.com>
2016-11-19 12:20:30 -0500

    Change id to contact_describe_id

--- src/java/echomarket/hibernate/ContactDescribes.java
+++ src/java/echomarket/hibernate/ContactDescribes.java
@@ -1,58 +1,62 @@
 package echomarket.hibernate;
-public class ContactDescribes  implements java.io.Serializable {
 
+public class ContactDescribes implements java.io.Serializable {
 
-     private String id;
-     private String purposeType;
-     private int optionValue;
-     private String borrowerOrLenderText;
+  private String contact_describe_id;
+  private String purposeType;
+  private int optionValue;
+  private String borrowerOrLenderText;
 
-    public ContactDescribes() {
-    }
+  public ContactDescribes() {
+  }
 
-	
-    public ContactDescribes(String id, int optionValue) {
-        this.id = id;
-        this.optionValue = optionValue;
-    }
-    public ContactDescribes(String id,  String borrowerOrLenderText, int optionValue) {
-       this.id = id;
-       this.optionValue = optionValue;
-       this.borrowerOrLenderText = borrowerOrLenderText;
-    }
-   
-    public String getId() {
-        return this.id;
-    }
-    
-    public void setId(String id) {
-        this.id = id;
-    }
-    public String getPurposeType() {
-        return this.purposeType;
-    }
-    
-    public void setPurposeType(String purposeType) {
-        this.purposeType = purposeType;
-    }
-    public int getOptionValue() {
-        return this.optionValue;
-    }
-    
-    public void setOptionValue(int optionValue) {
-        this.optionValue = optionValue;
-    }
-    public String getBorrowerOrLenderText() {
-        return this.borrowerOrLenderText;
-    }
-    
-    public void setBorrowerOrLenderText(String borrowerOrLenderText) {
-        this.borrowerOrLenderText = borrowerOrLenderText;
-    }
+  public ContactDescribes(String contact_describe_id, int optionValue) {
+    this.contact_describe_id = contact_describe_id;
+    this.optionValue = optionValue;
+  }
 
+  public ContactDescribes(String contact_describe_id, String borrowerOrLenderText, int optionValue) {
+    this.contact_describe_id = contact_describe_id;
+    this.optionValue = optionValue;
+    this.borrowerOrLenderText = borrowerOrLenderText;
+  }
 
+  public String getPurposeType() {
+    return this.purposeType;
+  }
 
+  public void setPurposeType(String purposeType) {
+    this.purposeType = purposeType;
+  }
+
+  public int getOptionValue() {
+    return this.optionValue;
+  }
+
+  public void setOptionValue(int optionValue) {
+    this.optionValue = optionValue;
+  }
+
+  public String getBorrowerOrLenderText() {
+    return this.borrowerOrLenderText;
+  }
+
+  public void setBorrowerOrLenderText(String borrowerOrLenderText) {
+    this.borrowerOrLenderText = borrowerOrLenderText;
+  }
+
+  /**
+   * @return the contact_describe_id
+   */
+  public String getContact_describe_id() {
+    return contact_describe_id;
+  }
+
+  /**
+   * @param contact_describe_id the contact_describe_id to set
+   */
+  public void setContact_describe_id(String contact_describe_id) {
+    this.contact_describe_id = contact_describe_id;
+  }
 
 }
-
-
