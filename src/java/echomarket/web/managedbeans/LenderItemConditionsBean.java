50962c76a99b8cd60a26ff46e86ad7b78dfb88b5
Liz McGurty <emcgurty2@gmail.com>
2016-11-29 06:31:03 -0500

    Making progress...

--- src/java/echomarket/web/managedbeans/LenderItemConditionsBean.java
+++ src/java/echomarket/web/managedbeans/LenderItemConditionsBean.java
@@ -54,77 +54,8 @@ public class LenderItemConditionsBean extends AbstractBean implements Serializab
 
   public LenderItemConditionsBean() {
   }
-
-  public String load_ud(String pid) {
-
-    List condList = null;
-    Map<String, String> params = null;
-    String strIid = null;
-    String action = null;
-
-    try {
-      params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-      strIid = params.get("iid");
-    } catch (Exception ex) {
-    }
-
-    try {
-      params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
-      action = params.get("action");
-    } catch (Exception ex) {
-    }
-
-    if (strIid == null) {
-      strIid = "";
-    }
-    if (strIid.isEmpty() == false) {
-      condList = getCurrentItemConditions_Iid(pid, strIid);
-    } else {
-      condList = getCurrentItemConditions(pid);
-    }
-    if (condList != null) {
-      if (condList.size() == 1) {
-        LenderItemConditions pp = (LenderItemConditions) condList.get(0);
-        if (pp != null) {
-          this.setItemId(pp.getItemId());
-          this.lender_item_condition_id = pp.getLender_item_condition_id();
-          this.participant_id = pp.getParticipant_id();
-          this.forFree = pp.getForFree();
-          this.availableForPurchase = pp.getAvailableForPurchase();
-          this.availableForPurchaseAmount = pp.getAvailableForPurchaseAmount();
-          this.smallFee = pp.getSmallFee();
-          this.smallFeeAmount = pp.getSmallFeeAmount();
-          this.availableForDonation = pp.getAvailableForDonation();
-          this.donateAnonymous = pp.getDonateAnonymous();
-          this.trade = pp.getTrade();
-          this.tradeItem = pp.getTradeItem();
-          this.agreedNumberOfDays = pp.getAgreedNumberOfDays();
-          this.agreedNumberOfHours = pp.getAgreedNumberOfHours();
-          this.indefiniteDuration = pp.getIndefiniteDuration();
-          this.presentDuringBorrowingPeriod = pp.getPresentDuringBorrowingPeriod();
-          this.entirePeriod = pp.getEntirePeriod();
-          this.partialPeriod = pp.getPartialPeriod();
-          this.provideProperUseTraining = pp.getProvideProperUseTraining();
-          this.specificConditions = pp.getSpecificConditions();
-          this.securityDepositAmount = pp.getSecurityDepositAmount();
-          this.securityDeposit = pp.getSecurityDeposit();
-//this.comment= pp.get(); // Later
-          pp = null;
-
-        }
-      } else if (condList.size() == 0) {
-        ubean.setEditable(1);
-      }
-    }
-    condList = null;
-    if ("edit".equals(action)) {
-      ubean.setEditable(1);
-    }
-
-    return "lender_conditions";
-  }
-
-  public List getCurrentItemConditions(String pid) {
+ 
+  private List getCurrentItemConditions(String pid) {
 
     List result = null;
     Session session = null;
@@ -135,7 +66,7 @@ public class LenderItemConditionsBean extends AbstractBean implements Serializab
       tx = session.beginTransaction();
       query = "from  LenderItemConditions lic "
               + " WHERE lic.participant_id = :pid"
-              + " ORDER BY lic.dateCreated ";
+              + " ORDER BY lic.participant_id, lic.dateCreated ";
       result = session.createQuery(query)
               .setParameter("pid", pid)
               .setMaxResults(1)
@@ -154,7 +85,7 @@ public class LenderItemConditionsBean extends AbstractBean implements Serializab
     return result;
   }
 
-  public List getCurrentItemConditions_Iid(String pid, String iid) {
+  private List getCurrentItemConditions_Iid(String pid, String iid) {
 
     List result = null;
     Session session = null;
@@ -165,7 +96,8 @@ public class LenderItemConditionsBean extends AbstractBean implements Serializab
       tx = session.beginTransaction();
       query = " from LenderItemConditions lic "
               + " WHERE lic.participant_id = :pid "
-              + " AND lic.itemId = :iid ";
+              + " AND lic.itemId = :iid "
+              + " ORDER BY lic.participant_id, lic.item_id, lic.dateCreated ";
       result = session.createQuery(query)
               .setParameter("pid", pid)
               .setParameter("iid", iid)
@@ -194,9 +126,44 @@ public class LenderItemConditionsBean extends AbstractBean implements Serializab
     List icList = null;
     Boolean successTransaction = false;
 
-    if (lender_item_condition_id.isEmpty() == false) {
+    Map<String, String> params = null;
+    String addNewCP = null;
+    try {
+      params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+      addNewCP = params.get("action");
+    } catch (Exception ex) {
+    }
 
-      icList = getCurrentItemConditions(ubean.getParticipant_id());
+    if (((lender_item_condition_id.isEmpty() == true) && (itemId == null)) && (addNewCP != null)) {
+
+      LenderItemConditions lic = new LenderItemConditions(getId(), ubean.getParticipant_id(), "NA", this.forFree, this.availableForPurchase, this.availableForPurchaseAmount, this.smallFee, this.smallFeeAmount, this.availableForDonation, this.donateAnonymous, this.trade, this.tradeItem, this.agreedNumberOfDays, this.agreedNumberOfHours, this.indefiniteDuration, this.presentDuringBorrowingPeriod, this.entirePeriod, this.partialPeriod, this.provideProperUseTraining, this.specificConditions, this.securityDepositAmount, this.securityDeposit, "NA", this.comment, new Date(), new Date());
+
+      try {
+        sb = hib_session();
+        tx = sb.beginTransaction();
+        sb.save(lic);
+        tx.commit();
+        message(null, "LenderItemConditionsSaved", null);
+        successTransaction = true;
+      } catch (Exception ex) {
+        tx.rollback();
+        System.out.println("Error in saveLenderItemCon");
+        Logger.getLogger(LenderItemConditionsBean.class.getName()).log(Level.SEVERE, null, ex);
+        message(null, "LenderItemConditionsNotSaved", null);
+        } finally {
+        sb = null;
+        tx = null;
+      }
+    } else {
+      
+    }
+      if ((addNewCP != null) && (itemId.isEmpty() == false)) {
+        icList = getCurrentItemConditions_Iid(ubean.getParticipant_id(), itemId);
+      } else {
+        icList = getCurrentItemConditions(ubean.getParticipant_id());
+      }
+      
+      if (icList != null) {
       if (icList.size() == 1) {
         LenderItemConditions ic = (LenderItemConditions) icList.get(0);
         ic.setItemId(itemId);
@@ -240,29 +207,8 @@ public class LenderItemConditionsBean extends AbstractBean implements Serializab
       } else {
         successTransaction = false;
       }
-
-    } else {
-      // Creating a new record
-      LenderItemConditions lic = new LenderItemConditions(getId(), ubean.getParticipant_id(), "NA", this.forFree, this.availableForPurchase, this.availableForPurchaseAmount, this.smallFee, this.smallFeeAmount, this.availableForDonation, this.donateAnonymous, this.trade, this.tradeItem, this.agreedNumberOfDays, this.agreedNumberOfHours, this.indefiniteDuration, this.presentDuringBorrowingPeriod, this.entirePeriod, this.partialPeriod, this.provideProperUseTraining, this.specificConditions, this.securityDepositAmount, this.securityDeposit, "NA", this.comment, new Date(), new Date());
-
-      try {
-        sb = hib_session();
-        tx = sb.beginTransaction();
-        sb.save(lic);
-        tx.commit();
-        message(null, "LenderItemConditionsSaved", null);
-        successTransaction = true;
-      } catch (Exception ex) {
-        tx.rollback();
-        System.out.println("Error in saveLenderItemCon");
-        Logger.getLogger(LenderItemConditionsBean.class.getName()).log(Level.SEVERE, null, ex);
-        message(null, "LenderItemConditionsNotSaved", null);
-        ubean.setEditable(11);
-      } finally {
-        sb = null;
-        tx = null;
       }
-    }
+    
 
     if (successTransaction == true) {
       ubean.setEditable(0);
@@ -271,6 +217,83 @@ public class LenderItemConditionsBean extends AbstractBean implements Serializab
     }
     return load_ud(ubean.getParticipant_id());
 
+  }
+  
+   public String load_ud(String pid) {
+
+    List condList = null;
+    Map<String, String> params = null;
+    String strIid = null;
+    String action = null;
+
+    try {
+      params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+      strIid = params.get("iid");
+    } catch (Exception ex) {
+    }
+    
+    if (ubean.getEditable() == 0) {
+      ubean.setEditable(1);
+    } else {
+      ubean.setEditable(0);
+    }
+
+    try {
+      params = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+      action = params.get("action");
+    } catch (Exception ex) {
+    }
+
+    if (strIid != null) {
+      condList = getCurrentItemConditions_Iid(pid, strIid);
+    } else {
+      strIid = itemId;
+      condList = getCurrentItemConditions_Iid(pid, itemId);
+    }
+     if (condList.size() == 0) {
+      condList = getCurrentItemConditions(pid);
+    }
+    
+    if (condList != null) {
+      if (condList.size() == 1) {
+        LenderItemConditions pp = (LenderItemConditions) condList.get(0);
+        if (pp != null) {
+          this.setItemId(pp.getItemId());
+          this.lender_item_condition_id = pp.getLender_item_condition_id();
+          this.participant_id = pp.getParticipant_id();
+          this.forFree = pp.getForFree();
+          this.availableForPurchase = pp.getAvailableForPurchase();
+          this.availableForPurchaseAmount = pp.getAvailableForPurchaseAmount();
+          this.smallFee = pp.getSmallFee();
+          this.smallFeeAmount = pp.getSmallFeeAmount();
+          this.availableForDonation = pp.getAvailableForDonation();
+          this.donateAnonymous = pp.getDonateAnonymous();
+          this.trade = pp.getTrade();
+          this.tradeItem = pp.getTradeItem();
+          this.agreedNumberOfDays = pp.getAgreedNumberOfDays();
+          this.agreedNumberOfHours = pp.getAgreedNumberOfHours();
+          this.indefiniteDuration = pp.getIndefiniteDuration();
+          this.presentDuringBorrowingPeriod = pp.getPresentDuringBorrowingPeriod();
+          this.entirePeriod = pp.getEntirePeriod();
+          this.partialPeriod = pp.getPartialPeriod();
+          this.provideProperUseTraining = pp.getProvideProperUseTraining();
+          this.specificConditions = pp.getSpecificConditions();
+          this.securityDepositAmount = pp.getSecurityDepositAmount();
+          this.securityDeposit = pp.getSecurityDeposit();
+//this.comment= pp.get(); // Later
+          pp = null;
+
+        }
+      } else if (condList.size() == 0) {
+        ubean.setEditable(1);
+      }
+    }
+    condList = null;
+    if ("edit".equals(action)) {
+      ubean.setEditable(1);
+    }
+
+    return "lender_conditions";
   }
 
   /**
