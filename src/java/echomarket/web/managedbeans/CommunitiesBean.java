1502148a8f7dc42dc2307275165c71b743632de2
Liz McGurty <emcgurty2@gmail.com>
2016-12-08 07:00:29 -0500

    progress

--- src/java/echomarket/web/managedbeans/CommunitiesBean.java
+++ src/java/echomarket/web/managedbeans/CommunitiesBean.java
@@ -1,7 +1,6 @@
 package echomarket.web.managedbeans;
 
 import echomarket.hibernate.Communities;
-import echomarket.hibernate.CommunityMembers;
 import javax.faces.bean.ManagedBean;
 import java.io.Serializable;
 import java.util.Date;
@@ -15,380 +14,388 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 @Named
-@ManagedBean(name = "cBean")
+@ManagedBean
 @RequestScoped
 public class CommunitiesBean extends AbstractBean implements Serializable {
 
-    @Inject
-    UserBean ubean;
-    private String communityName;
-    private Integer approved;
-    private String firstName;
-    private String mi;
-    private String lastName;
-    private String addressLine1;
-    private String addressLine2;
-    private String postalCode;
-    private String city;
-    private String province;
-    private String region;
-    private String usStateId;
-    private String countryId;
-    private String homePhone;
-    private String cellPhone;
-    private String email;
-    private Integer isActive;
-    private Integer isSaved;
-    private String remoteIp;
-    
-    public String load_community_detail() {
+  @Inject
+  UserBean ubean;
+  private String communityId;
+  private String communityName;
+  private Integer approved;
+  private String firstName;
+  private String mi;
+  private String lastName;
+  private String addressLine1;
+  private String addressLine2;
+  private String postalCode;
+  private String city;
+  private String province;
+  private String region;
+  private String usStateId;
+  private String countryId;
+  private String homePhone;
+  private String cellPhone;
+  private String email;
+  private Integer isActive;
+  private Integer isSaved;
+  private String remoteIp;
 
-        Session hib = hib_session();
-        Transaction tx = hib.beginTransaction();
-        String queryString = null;
-        List result = null;
-        Communities comm_Array = new Communities();
+  public String load_community_detail() {
 
-        if (ubean.getIsCommunity() == 1) {
-            queryString = "FROM Communities where community_id = :cid";
-            try {
-                result = hib.createQuery(queryString)
-                        .setParameter("cid", ubean.getUser_id())
-                        .list();
-                tx.commit();
+    List result = null;
+    Session hib = null;
+    Transaction tx = null;
 
-                if (result.size() > 0) {
-
-                    comm_Array = (Communities) result.get(0);
-
-                } else {
-                }
-            } catch (Exception ex) {
-
-            }
-
-            this.communityName = comm_Array.getCommunityName();
-            this.firstName = comm_Array.getFirstName();
-            this.lastName = comm_Array.getLastName();
-            this.email = ubean.getEmail();
-            if (!("NA".equals(comm_Array.getAddressLine1()))) {
-                this.addressLine1 = comm_Array.getAddressLine1();
-                this.addressLine2 = comm_Array.getAddressLine2();
-                this.postalCode = comm_Array.getPostalCode();
-                this.city = comm_Array.getCity();
-                this.province = comm_Array.getProvince();
-                this.region = comm_Array.getRegion();
-                this.usStateId = comm_Array.getUsStateId();
-                this.countryId = comm_Array.getCountryId();
-                this.homePhone = comm_Array.getHomePhone();
-                this.cellPhone = comm_Array.getCellPhone();
-                this.email = comm_Array.getEmail();
-
-            }
-
-        }
-
-        return "community_detail.xhtml?faces-redirect=true";
+    try {
+      hib = hib_session();
+      tx = hib.beginTransaction();
+    } catch (Exception ex) {
+      tx.rollback();
+    }
+    String queryString = null;
+    Communities comm_Array = new Communities();
+    queryString = "FROM Communities where community_id = :cid";
+    try {
+      result = hib.createQuery(queryString)
+              .setParameter("cid", ubean.getCommunityId())
+              .list();
+      tx.commit();
+    } catch (Exception ex) {
+      tx.rollback();
+      Logger.getLogger(CommunitiesBean.class.getName()).log(Level.SEVERE, null, ex);
+    } finally {
+      hib = null;
+      tx = null;
     }
 
+    if (result != null) {
+      if (result.size() == 1) {
+        comm_Array = (Communities) result.get(0);
+        this.communityId = comm_Array.getCommunityId();
+        this.communityName = comm_Array.getCommunityName();
+        this.firstName = comm_Array.getFirstName();
+        this.lastName = comm_Array.getLastName();
+        this.email = ubean.getEmail();
+        this.addressLine1 = comm_Array.getAddressLine1();
+        this.addressLine2 = comm_Array.getAddressLine2();
+        this.postalCode = comm_Array.getPostalCode();
+        this.city = comm_Array.getCity();
+        this.province = comm_Array.getProvince();
+        this.region = comm_Array.getRegion();
+        this.usStateId = comm_Array.getUsStateId();
+        this.countryId = comm_Array.getCountryId();
+        this.homePhone = comm_Array.getHomePhone();
+        this.cellPhone = comm_Array.getCellPhone();
+        this.email = comm_Array.getEmail();
 
-
-    public String saveCommunityDetail() {
-
-        Session sb = hib_session();
-        Transaction tx = sb.beginTransaction();
-        Date today_date = new Date();
-        Communities comm = new Communities(ubean.getUser_id(), this.communityName, 0, this.firstName, null, this.lastName,
-                this.addressLine1, this.addressLine2,
-                this.postalCode, this.city, this.province,
-                this.usStateId, this.countryId, this.homePhone, this.cellPhone, this.email, 1, 1, today_date, today_date, today_date, this.region, "NA");
-
-        try {
-            sb.update(comm);
-            tx.commit();
-
-            message(
-                    null,
-                    "CommunityDetailRecordSaved",
-                    null);
-        } catch (Exception ex) {
-            Logger.getLogger(CommunitiesBean.class.getName()).log(Level.SEVERE, null, ex);
-            message(
-                    null,
-                    "CommunityDetailRecordWasNotSaved",
-                    null);
-        }
-        sb = null;
-        tx = null;
-
-        return "index";
+      }
     }
 
-    /**
-     * @return the communityName
-     */
-    public String getCommunityName() {
-        return communityName;
-    }
+    return "community_detail.xhtml?faces-redirect=true";
+  }
 
-    /**
-     * @param communityName the communityName to set
-     */
-    public void setCommunityName(String communityName) {
-        this.communityName = communityName;
-    }
+  public String saveCommunityDetail() {
 
-    /**
-     * @return the approved
-     */
-    public Integer getApproved() {
-        return approved;
-    }
+    Session sb = null;
+    Transaction tx = null;
+    Communities comm = null;
 
-    /**
-     * @param approved the approved to set
-     */
-    public void setApproved(Integer approved) {
-        this.approved = approved;
+    try {
+      sb = hib_session();
+      tx = sb.beginTransaction();
+      comm = new Communities(getId(), this.communityName, 0, this.firstName, null, this.lastName, this.addressLine1, this.addressLine2, this.postalCode, this.city, this.province, this.usStateId, this.countryId, this.homePhone, this.cellPhone, this.email, 1, this.region, "NA");
+      sb.update(comm);
+      tx.commit();
+      message(null, "CommunityDetailRecordSaved", null);
+    } catch (Exception ex) {
+      Logger.getLogger(CommunitiesBean.class.getName()).log(Level.SEVERE, null, ex);
+      message(null, "CommunityDetailRecordWasNotSaved", null);
+    } finally {
+      sb = null;
+      tx = null;
     }
+    return "index";
+  }
 
-    /**
-     * @return the firstName
-     */
-    public String getFirstName() {
-        return firstName;
-    }
+  /**
+   * @return the communityName
+   */
+  public String getCommunityName() {
+    return communityName;
+  }
 
-    /**
-     * @param firstName the firstName to set
-     */
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
+  /**
+   * @param communityName the communityName to set
+   */
+  public void setCommunityName(String communityName) {
+    this.communityName = communityName;
+  }
 
-    /**
-     * @return the mi
-     */
-    public String getMi() {
-        return mi;
-    }
+  /**
+   * @return the approved
+   */
+  public Integer getApproved() {
+    return approved;
+  }
 
-    /**
-     * @param mi the mi to set
-     */
-    public void setMi(String mi) {
-        this.mi = mi;
-    }
+  /**
+   * @param approved the approved to set
+   */
+  public void setApproved(Integer approved) {
+    this.approved = approved;
+  }
 
-    /**
-     * @return the lastName
-     */
-    public String getLastName() {
-        return lastName;
-    }
+  /**
+   * @return the firstName
+   */
+  public String getFirstName() {
+    return firstName;
+  }
 
-    /**
-     * @param lastName the lastName to set
-     */
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+  /**
+   * @param firstName the firstName to set
+   */
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
 
-    /**
-     * @return the addressLine1
-     */
-    public String getAddressLine1() {
-        return addressLine1;
-    }
+  /**
+   * @return the mi
+   */
+  public String getMi() {
+    return mi;
+  }
 
-    /**
-     * @param addressLine1 the addressLine1 to set
-     */
-    public void setAddressLine1(String addressLine1) {
-        this.addressLine1 = addressLine1;
-    }
+  /**
+   * @param mi the mi to set
+   */
+  public void setMi(String mi) {
+    this.mi = mi;
+  }
 
-    /**
-     * @return the addressLine2
-     */
-    public String getAddressLine2() {
-        return addressLine2;
-    }
+  /**
+   * @return the lastName
+   */
+  public String getLastName() {
+    return lastName;
+  }
 
-    /**
-     * @param addressLine2 the addressLine2 to set
-     */
-    public void setAddressLine2(String addressLine2) {
-        this.addressLine2 = addressLine2;
-    }
+  /**
+   * @param lastName the lastName to set
+   */
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
 
-    /**
-     * @return the postalCode
-     */
-    public String getPostalCode() {
-        return postalCode;
-    }
+  /**
+   * @return the addressLine1
+   */
+  public String getAddressLine1() {
+    return addressLine1;
+  }
 
-    /**
-     * @param postalCode the postalCode to set
-     */
-    public void setPostalCode(String postalCode) {
-        this.postalCode = postalCode;
-    }
+  /**
+   * @param addressLine1 the addressLine1 to set
+   */
+  public void setAddressLine1(String addressLine1) {
+    this.addressLine1 = addressLine1;
+  }
 
-    /**
-     * @return the city
-     */
-    public String getCity() {
-        return city;
-    }
+  /**
+   * @return the addressLine2
+   */
+  public String getAddressLine2() {
+    return addressLine2;
+  }
 
-    /**
-     * @param city the city to set
-     */
-    public void setCity(String city) {
-        this.city = city;
-    }
+  /**
+   * @param addressLine2 the addressLine2 to set
+   */
+  public void setAddressLine2(String addressLine2) {
+    this.addressLine2 = addressLine2;
+  }
 
-    /**
-     * @return the province
-     */
-    public String getProvince() {
-        return province;
-    }
+  /**
+   * @return the postalCode
+   */
+  public String getPostalCode() {
+    return postalCode;
+  }
 
-    /**
-     * @param province the province to set
-     */
-    public void setProvince(String province) {
-        this.province = province;
-    }
+  /**
+   * @param postalCode the postalCode to set
+   */
+  public void setPostalCode(String postalCode) {
+    this.postalCode = postalCode;
+  }
 
-    /**
-     * @return the usStateId
-     */
-    public String getUsStateId() {
-        return usStateId;
-    }
+  /**
+   * @return the city
+   */
+  public String getCity() {
+    return city;
+  }
 
-    /**
-     * @param usStateId the usStateId to set
-     */
-    public void setUsStateId(String usStateId) {
-        this.usStateId = usStateId;
-    }
+  /**
+   * @param city the city to set
+   */
+  public void setCity(String city) {
+    this.city = city;
+  }
 
-    /**
-     * @return the countryId
-     */
-    public String getCountryId() {
-        return countryId;
-    }
+  /**
+   * @return the province
+   */
+  public String getProvince() {
+    return province;
+  }
 
-    /**
-     * @param countryId the countryId to set
-     */
-    public void setCountryId(String countryId) {
-        this.countryId = countryId;
-    }
+  /**
+   * @param province the province to set
+   */
+  public void setProvince(String province) {
+    this.province = province;
+  }
 
-    /**
-     * @return the homePhone
-     */
-    public String getHomePhone() {
-        return homePhone;
-    }
+  /**
+   * @return the usStateId
+   */
+  public String getUsStateId() {
+    return usStateId;
+  }
 
-    /**
-     * @param homePhone the homePhone to set
-     */
-    public void setHomePhone(String homePhone) {
-        this.homePhone = homePhone;
-    }
+  /**
+   * @param usStateId the usStateId to set
+   */
+  public void setUsStateId(String usStateId) {
+    this.usStateId = usStateId;
+  }
 
-    /**
-     * @return the cellPhone
-     */
-    public String getCellPhone() {
-        return cellPhone;
-    }
+  /**
+   * @return the countryId
+   */
+  public String getCountryId() {
+    return countryId;
+  }
 
-    /**
-     * @param cellPhone the cellPhone to set
-     */
-    public void setCellPhone(String cellPhone) {
-        this.cellPhone = cellPhone;
-    }
+  /**
+   * @param countryId the countryId to set
+   */
+  public void setCountryId(String countryId) {
+    this.countryId = countryId;
+  }
 
-    /**
-     * @return the email
-     */
-    public String getEmail() {
-        return email;
-    }
+  /**
+   * @return the homePhone
+   */
+  public String getHomePhone() {
+    return homePhone;
+  }
 
-    /**
-     * @param email the email to set
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  /**
+   * @param homePhone the homePhone to set
+   */
+  public void setHomePhone(String homePhone) {
+    this.homePhone = homePhone;
+  }
 
-    /**
-     * @return the isActive
-     */
-    public Integer getIsActive() {
-        return isActive;
-    }
+  /**
+   * @return the cellPhone
+   */
+  public String getCellPhone() {
+    return cellPhone;
+  }
 
-    /**
-     * @param isActive the isActive to set
-     */
-    public void setIsActive(Integer isActive) {
-        this.isActive = isActive;
-    }
+  /**
+   * @param cellPhone the cellPhone to set
+   */
+  public void setCellPhone(String cellPhone) {
+    this.cellPhone = cellPhone;
+  }
 
-    /**
-     * @return the isSaved
-     */
-    public Integer getIsSaved() {
-        return isSaved;
-    }
+  /**
+   * @return the email
+   */
+  public String getEmail() {
+    return email;
+  }
 
-    /**
-     * @param isSaved the isSaved to set
-     */
-    public void setIsSaved(Integer isSaved) {
-        this.isSaved = isSaved;
-    }
+  /**
+   * @param email the email to set
+   */
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-    /**
-     * @return the remoteIp
-     */
-    public String getRemoteIp() {
-        return remoteIp;
-    }
+  /**
+   * @return the isActive
+   */
+  public Integer getIsActive() {
+    return isActive;
+  }
 
-    /**
-     * @param remoteIp the remoteIp to set
-     */
-    public void setRemoteIp(String remoteIp) {
-        this.remoteIp = remoteIp;
-    }
+  /**
+   * @param isActive the isActive to set
+   */
+  public void setIsActive(Integer isActive) {
+    this.isActive = isActive;
+  }
 
-    /**
-     * @return the region
-     */
-    public String getRegion() {
-        return region;
-    }
+  /**
+   * @return the isSaved
+   */
+  public Integer getIsSaved() {
+    return isSaved;
+  }
 
-    /**
-     * @param region the region to set
-     */
-    public void setRegion(String region) {
-        this.region = region;
-    }
+  /**
+   * @param isSaved the isSaved to set
+   */
+  public void setIsSaved(Integer isSaved) {
+    this.isSaved = isSaved;
+  }
 
+  /**
+   * @return the remoteIp
+   */
+  public String getRemoteIp() {
+    return remoteIp;
+  }
+
+  /**
+   * @param remoteIp the remoteIp to set
+   */
+  public void setRemoteIp(String remoteIp) {
+    this.remoteIp = remoteIp;
+  }
+
+  /**
+   * @return the region
+   */
+  public String getRegion() {
+    return region;
+  }
+
+  /**
+   * @param region the region to set
+   */
+  public void setRegion(String region) {
+    this.region = region;
+  }
+
+  /**
+   * @return the communityId
+   */
+  public String getCommunityId() {
+    return communityId;
+  }
+
+  /**
+   * @param communityId the communityId to set
+   */
+  public void setCommunityId(String communityId) {
+    this.communityId = communityId;
+  }
 
 }
