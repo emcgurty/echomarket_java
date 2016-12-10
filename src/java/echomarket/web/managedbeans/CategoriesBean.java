e15974f8ebd7c8bbabf847aa5701b2f9fd4d4db1
Liz McGurty <emcgurty2@gmail.com>
2016-12-10 07:55:33 -0500

    not null

--- src/java/echomarket/web/managedbeans/CategoriesBean.java
+++ src/java/echomarket/web/managedbeans/CategoriesBean.java
@@ -19,105 +19,104 @@ import org.hibernate.Transaction;
 @RequestScoped
 public class CategoriesBean extends AbstractBean implements Serializable {
 
-    private Integer id;
-    private String categoryType;
+  private Integer id;
+  private String categoryType;
 
-    public CategoriesBean() {
+  public CategoriesBean() {
+  }
+
+  public String getCategoryType() {
+    return categoryType;
+  }
+
+  /**
+   * @param categoryType the categoryType to set
+   */
+  public void setCategoryType(String categoryType) {
+    this.categoryType = categoryType;
+  }
+
+  public String getCategoryName(Integer cid) {
+    String returnString = null;
+    List result = null;
+    Session session;
+    Transaction tx;
+    session = null;
+    tx = null;
+    
+    try {
+      session = hib_session();
+      tx = session.beginTransaction();
+      result = session.createQuery("from Categories WHERE id = :cid")
+              .setParameter("cid", cid)
+              .list();
+      tx.commit();
+    } catch (Exception ex) {
+      tx.rollback();
+      System.out.println("Error in getCategoryName");
+      Logger.getLogger(CategoriesBean.class.getName()).log(Level.SEVERE, null, ex);
+    } finally {
+      session = null;
+      tx = null;
+    }
+    if (result != null) {
+      if (result.size() == 1) {
+        Categories retCat = (Categories) result.get((0));
+        returnString = retCat.getCategoryType();
+      }
+    } else {
+      returnString = "Not found";
     }
 
-    public String getCategoryType() {
-        return categoryType;
+    result = null;
+    return returnString;
+  }
+
+  public Categories[] buildCatArray() {
+    Categories[] catArray = null;
+    List cat_list = null;
+    cat_list = cat_list();
+    if (cat_list != null) {
+      int size_of_list = cat_list.size();
+      catArray = new Categories[size_of_list];
+      for (int i = 0; i < size_of_list; i++) {
+        Categories cArray = (Categories) cat_list.get(i);
+        catArray[i] = new Categories(cArray.getId(), cArray.getCategoryType());
+      }
+    }
+    return catArray;
+  }
+
+  private List cat_list() {
+
+    List result = null;
+    Session session;
+    Transaction tx;
+    session = null;
+    tx = null;
+
+    try {
+      session = hib_session();
+      tx = session.beginTransaction();
+    } catch (Exception ex) {
+      System.out.println("Error in establishing transaction in cat_list");
+      Logger.getLogger(CategoriesBean.class.getName()).log(Level.SEVERE, null, ex);
+      tx.rollback();
     }
 
-    /**
-     * @param categoryType the categoryType to set
-     */
-    public void setCategoryType(String categoryType) {
-        this.categoryType = categoryType;
+    try {
+      result = session.createQuery("from Categories Order By id").list();
+      tx.commit();
+    } catch (Exception e) {
+      tx.rollback();
+      System.out.println("Error line 108 CatBean");
+      e.printStackTrace();
+
     }
+    session = null;
+    tx = null;
 
-    public String getCategoryName(Integer cid) {
-        String returnString = null;
-        List result = null;
-        Session session;
-        Transaction tx;
-        session = null;
-        tx = null;
-        session = hib_session();
-        
-        try {
-            tx = session.beginTransaction();
-            result = session.createQuery("from Categories WHERE id = :cid")
-                    .setParameter("cid", cid)
-                    .list();
-            tx.commit();
-        } catch (Exception ex) {
-            tx.rollback();
-            System.out.println("Error in getCategoryName");
-            Logger.getLogger(CategoriesBean.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            session = null;
-            tx = null;
-        }
-
-        if (result.size() == 1) {
-            Categories retCat = (Categories) result.get((0));
-            returnString = retCat.getCategoryType();
-
-        } else {
-
-            returnString = "Not found";
-        }
-
-        result = null;
-        return returnString;
-    }
-
-    public Categories[] buildCatArray() {
-        Categories[] catArray = null;
-        List cat_list = null;
-        cat_list = cat_list();
-        if (cat_list != null) {
-            int size_of_list = cat_list.size();
-            catArray = new Categories[size_of_list];
-            for (int i = 0; i < size_of_list; i++) {
-                Categories cArray = (Categories) cat_list.get(i);
-                catArray[i] = new Categories(cArray.getId(), cArray.getCategoryType());
-            }
-        }
-        return catArray;
-    }
-
-    private List cat_list() {
-
-        List result = null;
-        Session session;
-        Transaction tx;
-        session = null;
-        tx = null;
-
-        try {
-            session = hib_session();
-            tx = session.beginTransaction();
-        } catch (Exception ex) {
-            System.out.println("Error in establishing transaction in cat_list");
-            Logger.getLogger(CategoriesBean.class.getName()).log(Level.SEVERE, null, ex);
-            tx.rollback();
-        }
-
-        try {
-            result = session.createQuery("from Categories Order By id").list();
-            tx.commit();
-        } catch (Exception e) {
-            tx.rollback();
-            System.out.println("Error line 108 CatBean");
-            e.printStackTrace();
-
-        }
-        session = null;
-        tx = null;
-
-        return result;
-    }
+    return result;
+  }
 
 }
