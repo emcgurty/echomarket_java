d03884ee230c2c294cb0fdb04d8d15b52145cf93
Liz McGurty <emcgurty2@gmail.com>
2016-12-10 07:55:19 -0500

    not null

--- src/java/echomarket/web/managedbeans/ItemConditionBean.java
+++ src/java/echomarket/web/managedbeans/ItemConditionBean.java
@@ -18,92 +18,90 @@ import org.hibernate.Transaction;
 public class ItemConditionBean extends AbstractBean implements Serializable {
 
 //    private Integer id;
-    private String condition;
+  private String condition;
 
-    public ItemConditionBean() {
+  public ItemConditionBean() {
+  }
+
+  public String getItemConditionName(Integer cid) {
+
+    String returnString = null;
+    List result = null;
+    Session session = null;
+    Transaction tx = null;
+
+    try {
+      session = hib_session();
+      tx = session.beginTransaction();
+      result = session.createQuery("from ItemConditions WHERE id = :cid")
+              .setParameter("cid", cid)
+              .list();
+      tx.commit();
+    } catch (Exception ex) {
+      tx.rollback();
+      System.out.println("Error in getItemConditionName");
+      Logger.getLogger(ItemConditionBean.class.getName()).log(Level.SEVERE, null, ex);
+    } finally {
+      session = null;
+      tx = null;
+    }
+    if (result != null) {
+      if (result.size() == 1) {
+        ItemConditions retCat = (ItemConditions) result.get((0));
+        returnString = retCat.getCondition();
+        retCat = null;
+      }
+    } else {
+      returnString = "Not found";
     }
 
-    public String getItemConditionName(Integer cid) {
+    result = null;
+    return returnString;
+  }
 
-        String returnString = null;
-        List result = null;
-        Session session;
-        Transaction tx;
-        session = null;
-        tx = null;
-        session = hib_session();
-
-        try {
-            tx = session.beginTransaction();
-            result = session.createQuery("from ItemConditions WHERE id = :cid")
-                    .setParameter("cid", cid)
-                    .list();
-            tx.commit();
-        } catch (Exception ex) {
-            tx.rollback();
-            System.out.println("Error in getItemConditionName");
-            Logger.getLogger(ItemConditionBean.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            session = null;
-            tx = null;
-        }
-
-        if (result.size() == 1) {
-            ItemConditions retCat = (ItemConditions) result.get((0));
-            returnString = retCat.getCondition();
-
-        } else {
-
-            returnString = "Not found";
-        }
-
-        result = null;
-        return returnString;
+  public ItemConditions[] buildItemConditionsArray() {
+    ItemConditions[] catArray = null;
+    List cat_list = null;
+    cat_list = cat_list();
+    int size_of_list = cat_list.size();
+    catArray = new ItemConditions[size_of_list];
+    for (int i = 0; i < size_of_list; i++) {
+      ItemConditions cArray = (ItemConditions) cat_list.get(i);
+      catArray[i] = new ItemConditions(cArray.getId(), cArray.getCondition());
     }
+    return catArray;
+  }
 
-    public ItemConditions[] buildItemConditionsArray() {
-        ItemConditions[] catArray = null;
-        List cat_list = null;
-        cat_list = cat_list();
-        int size_of_list = cat_list.size();
-        catArray = new ItemConditions[size_of_list];
-        for (int i = 0; i < size_of_list; i++) {
-            ItemConditions cArray = (ItemConditions) cat_list.get(i);
-            catArray[i] = new ItemConditions(cArray.getId(), cArray.getCondition());
-        }
-        return catArray;
+  private List cat_list() {
+
+    List result = null;
+    Session session = hib_session();
+    Transaction tx = session.beginTransaction();
+
+    try {
+      result = session.createQuery("from ItemConditions order by id").list();
+    } catch (Exception e) {
+      System.out.println("Error line 48 Itemm Conditions");
+      e.printStackTrace();
+
     }
+    tx.commit();
 
-    private List cat_list() {
+    return result;
+  }
 
-        List result = null;
-        Session session = hib_session();
-        Transaction tx = session.beginTransaction();
+  /**
+   * @return the condition
+   */
+  public String getCondition() {
+    return condition;
+  }
 
-        try {
-            result = session.createQuery("from ItemConditions order by id").list();
-        } catch (Exception e) {
-            System.out.println("Error line 48 Itemm Conditions");
-            e.printStackTrace();
-
-        }
-        tx.commit();
-
-        return result;
-    }
-
-    /**
-     * @return the condition
-     */
-    public String getCondition() {
-        return condition;
-    }
-
-    /**
-     * @param condition the condition to set
-     */
-    public void setCondition(String condition) {
-        this.condition = condition;
-    }
+  /**
+   * @param condition the condition to set
+   */
+  public void setCondition(String condition) {
+    this.condition = condition;
+  }
 
 }
