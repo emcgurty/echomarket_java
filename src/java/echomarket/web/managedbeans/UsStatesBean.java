4dbbf334caeb4b76b3c0212ae862a508c1f2324d
Liz McGurty <emcgurty2@gmail.com>
2016-12-18 10:39:33 -0500

    old code from early learning

--- src/java/echomarket/web/managedbeans/UsStatesBean.java
+++ src/java/echomarket/web/managedbeans/UsStatesBean.java
@@ -14,165 +14,129 @@ import org.hibernate.Transaction;
 @RequestScoped
 public class UsStatesBean extends AbstractBean implements Serializable {
 
-    private String id;
-    private String stateName;
-    private Boolean us_built;
+  private String id;
+  private String stateName;
+  private Boolean us_built;
 
-    public UsStatesBean() {
+  public UsStatesBean() {
+  }
+
+  public String getOneState(String one_state) {
+
+    List result = null;
+    Session session = null;
+    Transaction tx = null;
+    String returnState = null;
+
+    try {
+      session = hib_session();
+      tx = session.beginTransaction();
+      result = session.createQuery("from UsStates WHERE id = :one_state")
+              .setParameter("one_state", one_state)
+              .list();
+      tx.commit();
+    } catch (Exception e) {
+      tx.rollback();
+      System.out.println("Error at line 36 in US Bean");
+      e.printStackTrace();
+
+    } finally {
+      session = null;
+      tx = null;
+
     }
 
-    public String getOneState(String one_state) {
-
-        List result = null;
-        Session session;
-        Transaction tx;
-        session = null;
-        tx = null;
-
-        try {
-            session = hib_session();
-            tx = session.beginTransaction();
-        } catch (Exception ex) {
-            System.out.println("Error at line 33 in US Bean");
-            ex.printStackTrace();
-            message(
-                    null,
-                    "ApplicationError",
-                    new Object[]{ex});
-            return "index";
-        }
-
-        String returnState = null;
-
-        try {
-            result = session.createQuery("from UsStates WHERE id = :one_state")
-                    .setParameter("one_state", one_state)
-                    .list();
-            tx.commit();
-
-        } catch (Exception e) {
-            tx.rollback();
-            System.out.println("Error at line 36 in US Bean");
-            e.printStackTrace();
-
-        } finally {
-            //session.close();
-            System.out.println("IS TX STILL ACTIVE 58");
-            System.out.println(tx.isActive());
-            System.out.println("IS TX STILL ACTIVE 58 - close");
-            session = null;
-            tx = null;
-
-        }
-
-        if (result.size() > 0) {
-            UsStates returnedStateName = (UsStates) result.get(0);
-            returnState = returnedStateName.getStateName();
-            returnedStateName = null;
-        } else {
-            returnState = "State not found";
-        }
-
-        result = null;
-        return returnState;
+    if (result.size() > 0) {
+      UsStates returnedStateName = (UsStates) result.get(0);
+      returnState = returnedStateName.getStateName();
+      returnedStateName = null;
+    } else {
+      returnState = "State not found";
     }
 
-    public UsStates[] buildUsStates() {
+    result = null;
+    return returnState;
+  }
 
-        UsStates[] purposeArray = null;
-        List uslist = null;
-        uslist = us_list();
-        int size_of_list = uslist.size();
-        purposeArray = new UsStates[size_of_list];
-        for (int i = 0; i < size_of_list; i++) {
-            UsStates to_Array = (UsStates) uslist.get(i);
-            purposeArray[i] = new UsStates(to_Array.getId(), to_Array.getStateName());
-        }
+  public UsStates[] buildUsStates() {
 
-        uslist = null;
-        return purposeArray;
+    UsStates[] purposeArray = null;
+    List uslist = null;
+    uslist = us_list();
+    int size_of_list = uslist.size();
+    purposeArray = new UsStates[size_of_list];
+    for (int i = 0; i < size_of_list; i++) {
+      UsStates to_Array = (UsStates) uslist.get(i);
+      purposeArray[i] = new UsStates(to_Array.getId(), to_Array.getStateName());
     }
 
-    private List us_list() {
+    uslist = null;
+    return purposeArray;
+  }
 
-        System.out.println("US LIST CALL");
-        List result = null;
+  private List us_list() {
 
-        Session session = hib_session();
-        Transaction tx = null;
-        try {
-            tx = session.beginTransaction();
-
-            try {
-                result = session.createQuery("FROM UsStates ORDER BY id").list();
-                tx.commit();
-            } catch (Exception e) {
-                System.out.println("Error at line 99 in US Bean");
-                e.printStackTrace();
-                if (tx != null) {
-                    tx.rollback();
-                }
-            } finally {
-            System.out.println("IS TX STILL ACTIVE 114");
-            System.out.println(tx.isActive());
-            System.out.println("IS TX STILL ACTIVE 114 - close");
-            }
-
-        } catch (RuntimeException e) {
-                System.out.println("Error at line 96 in US Bean");
-                e.printStackTrace();
-        } finally {
-            if (session.isOpen() == true) session.close();
-            System.out.println("IS TX STILL ACTIVE 124");
-            System.out.println(tx.isActive());
-            System.out.println("IS TX STILL ACTIVE 124- close");
-            session = null;
-            tx = null;
-        }
-
-        return result;
+    System.out.println("US LIST CALL");
+    List result = null;
+    Session session = null;
+    Transaction tx = null;
+    try {
+      session = hib_session();
+      tx = session.beginTransaction();
+      result = session.createQuery("FROM UsStates ORDER BY id").list();
+      tx.commit();
+    } catch (Exception e) {
+      tx.rollback();
+      System.out.println("Error at line 99 in US Bean");
+      e.printStackTrace();
+    } finally {
+      tx = null;
+      session = null;
     }
 
-    /**
-     * @return the id
-     */
-    public String getId() {
-        return id;
-    }
+    return result;
+  }
 
-    /**
-     * @param id the id to set
-     */
-    public void setId(String id) {
-        this.id = id;
-    }
+  /**
+   * @return the id
+   */
+  public String getId() {
+    return id;
+  }
 
-    /**
-     * @return the stateName
-     */
-    public String getStateName() {
-        return stateName;
-    }
+  /**
+   * @param id the id to set
+   */
+  public void setId(String id) {
+    this.id = id;
+  }
 
-    /**
-     * @param stateName the stateName to set
-     */
-    public void setStateName(String stateName) {
-        this.stateName = stateName;
-    }
+  /**
+   * @return the stateName
+   */
+  public String getStateName() {
+    return stateName;
+  }
 
-    /**
-     * @return the us_built
-     */
-    public Boolean getUs_built() {
-        return us_built;
-    }
+  /**
+   * @param stateName the stateName to set
+   */
+  public void setStateName(String stateName) {
+    this.stateName = stateName;
+  }
 
-    /**
-     * @param us_built the us_built to set
-     */
-    public void setUs_built(Boolean us_built) {
-        this.us_built = us_built;
-    }
+  /**
+   * @return the us_built
+   */
+  public Boolean getUs_built() {
+    return us_built;
+  }
+
+  /**
+   * @param us_built the us_built to set
+   */
+  public void setUs_built(Boolean us_built) {
+    this.us_built = us_built;
+  }
 
 }
