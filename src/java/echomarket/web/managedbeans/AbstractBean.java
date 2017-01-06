2aad2a3cbfafb449ea40483f229563ea194b755f
Liz McGurty <emcgurty2@gmail.com>
2017-01-06 14:26:25 -0500

    Getting Netbean warning that element is not a managed bean.  Otherwise, working toward learning remote IP Address.

--- src/java/echomarket/web/managedbeans/AbstractBean.java
+++ src/java/echomarket/web/managedbeans/AbstractBean.java
@@ -1,10 +1,3 @@
-/*
- * Copyright 2012 Oracle and/or its affiliates.
- * All rights reserved.  You may not modify, use,
- * reproduce, or distribute this software except in
- * compliance with  the terms of the License at:
- * http://developers.sun.com/license/berkeley_license.html
- */
 package echomarket.web.managedbeans;
 
 import echomarket.hibernate.HibernateUtil;
@@ -30,155 +23,161 @@ public abstract class AbstractBean implements Serializable {
 
 //  
 //    Source:  Setting UUID ids: http://blog.xebia.com/jpa-implementation-patterns-using-uuids-as-primary-keys/
+  private static final long serialVersionUID = 1L;
 
-    private static final long serialVersionUID = 1L;
+  @Id
+  private String id;
+ 
+  public AbstractBean() {
+    this.id = UUID.randomUUID().toString();
+  }
 
-    @Id
-    private String id;
+  @Override
+  public int hashCode() {
+    return getId().hashCode();
+  }
 
-    public AbstractBean() {
-        this.id = UUID.randomUUID().toString();
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (!(obj instanceof AbstractBean)) {
+      return false;
+    }
+    AbstractBean other = (AbstractBean) obj;
+    return getId().equals(other.getId());
+  }
+
+  /**
+   * @return the id
+   */
+  protected String getId() {
+    return id;
+  }
+
+  /**
+   * @param id the id to set
+   */
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  protected FacesContext context() {
+    return (FacesContext.getCurrentInstance());
+  }
+
+  protected Session hib_session() {
+    return HibernateUtil.getSessionFactory().getCurrentSession();
+
+  }
+
+  protected void message(String clientId, String key) {
+    // Look up the requested message text
+    String text = "";
+
+    try {
+      ResourceBundle bundle = ResourceBundle.getBundle(
+              "echomarket.web.messages.Messages",
+              context().getViewRoot().getLocale());
+      text = bundle.getString(key);
+    } catch (Exception e) {
+      text = "???" + key + "???";
     }
 
-    @Override
-    public int hashCode() {
-        return getId().hashCode();
+    // Construct and add a FacesMessage containing it
+    context()
+            .addMessage(
+                    clientId,
+                    new FacesMessage(text));
+  }
+
+  /**
+   * <p>
+   * Add a localized message to the <code>FacesContext</code> for the current
+   * request.</p>
+   *
+   * @param clientId Client identifier of the component this message relates to,
+   * or <code>null</code> for global messages
+   * @param key Message key of the message to include
+   * @param params Substitution parameters for using the localized text as a
+   * message format
+   */
+  protected void message(
+          String clientId,
+          String key,
+          Object[] params) {
+    // Look up the requested message text
+    String text = "";
+
+    try {
+      ResourceBundle bundle = ResourceBundle.getBundle(
+              "echomarket.web.messages.Messages",
+              context().getViewRoot().getLocale());
+      text = bundle.getString(key);
+    } catch (Exception e) {
+      text = "???" + key + "???";
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (!(obj instanceof AbstractBean)) {
-            return false;
-        }
-        AbstractBean other = (AbstractBean) obj;
-        return getId().equals(other.getId());
+    // Perform the requested substitutions
+    if ((params != null) && (params.length > 0)) {
+      text = MessageFormat.format(text, params);
     }
 
-    /**
-     * @return the id
-     */
-    protected String getId() {
-        return id;
+    // Construct and add a FacesMessage containing it
+    context()
+            .addMessage(
+                    clientId,
+                    new FacesMessage(text));
+  }
+
+  public static String getClientIpAddr() {
+    HttpServletRequest request = (HttpServletRequest) (FacesContext.getCurrentInstance().
+            getExternalContext().getRequest());
+    request.getHeaderNames();
+    request.getRemoteAddr();
+    request.getRemoteHost();
+    String ipAddress = request.getHeader("X-FORWARDED-FOR");
+    if (ipAddress == null) {
+      ipAddress = request.getRemoteAddr();
     }
-
-    /**
-     * @param id the id to set
-     */
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    protected FacesContext context() {
-        return (FacesContext.getCurrentInstance());
-    }
-
-    protected Session hib_session() {
-        return HibernateUtil.getSessionFactory().getCurrentSession();
-
-    }
-
-    protected void message( String clientId, String key) {
-        // Look up the requested message text
-        String text = "";
-
-        try {
-            ResourceBundle bundle = ResourceBundle.getBundle(
-                    "echomarket.web.messages.Messages",
-                    context().getViewRoot().getLocale());
-            text = bundle.getString(key);
-        } catch (Exception e) {
-            text = "???" + key + "???";
-        }
-
-        // Construct and add a FacesMessage containing it
-        context()
-                .addMessage(
-                        clientId,
-                        new FacesMessage(text));
-    }
-
-    /**
-     * <p>
-     * Add a localized message to the <code>FacesContext</code> for the current
-     * request.</p>
-     *
-     * @param clientId Client identifier of the component this message relates
-     * to, or <code>null</code> for global messages
-     * @param key Message key of the message to include
-     * @param params Substitution parameters for using the localized text as a
-     * message format
-     */
-    protected void message(
-            String clientId,
-            String key,
-            Object[] params) {
-        // Look up the requested message text
-        String text = "";
-
-        try {
-            ResourceBundle bundle = ResourceBundle.getBundle(
-                    "echomarket.web.messages.Messages",
-                    context().getViewRoot().getLocale());
-            text = bundle.getString(key);
-        } catch (Exception e) {
-            text = "???" + key + "???";
-        }
-
-        // Perform the requested substitutions
-        if ((params != null) && (params.length > 0)) {
-            text = MessageFormat.format(text, params);
-        }
-
-        // Construct and add a FacesMessage containing it
-        context()
-                .addMessage(
-                        clientId,
-                        new FacesMessage(text));
-    }
-    
-    public static String getClientIpAddr(HttpServletRequest request) {  
-    String ip = request.getHeader("X-Forwarded-For");  
-    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-        ip = request.getHeader("Proxy-Client-IP");  
-    }  
-    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-        ip = request.getHeader("WL-Proxy-Client-IP");  
-    }  
-    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-        ip = request.getHeader("HTTP_X_FORWARDED_FOR");  
-    }  
-    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-        ip = request.getHeader("HTTP_X_FORWARDED");  
-    }  
-    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-        ip = request.getHeader("HTTP_X_CLUSTER_CLIENT_IP");  
-    }  
-    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-        ip = request.getHeader("HTTP_CLIENT_IP");  
-    }  
-    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-        ip = request.getHeader("HTTP_FORWARDED_FOR");  
-    }  
-    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-        ip = request.getHeader("HTTP_FORWARDED");  
-    }  
-    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-        ip = request.getHeader("HTTP_VIA");  
-    }  
-    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-        ip = request.getHeader("REMOTE_ADDR");  
-    }  
-    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-        ip = request.getRemoteAddr();  
-    }  
-    return ip;  
-}
-
-    
+//    String ip = request.getHeader("X-Forwarded-For");
+//    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//      ip = request.getHeader("Proxy-Client-IP");
+//    }
+//    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//      ip = request.getHeader("WL-Proxy-Client-IP");
+//    }
+//    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//      ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+//    }
+//    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//      ip = request.getHeader("HTTP_X_FORWARDED");
+//    }
+//    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//      ip = request.getHeader("HTTP_X_CLUSTER_CLIENT_IP");
+//    }
+//    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//      ip = request.getHeader("HTTP_CLIENT_IP");
+//    }
+//    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//      ip = request.getHeader("HTTP_FORWARDED_FOR");
+//    }
+//    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//      ip = request.getHeader("HTTP_FORWARDED");
+//    }
+//    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//      ip = request.getHeader("HTTP_VIA");
+//    }
+//    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//      ip = request.getHeader("REMOTE_ADDR");
+//    }
+//    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+//      ip = request.getRemoteAddr();
+//    }
+    return ipAddress;
+  }
 }
