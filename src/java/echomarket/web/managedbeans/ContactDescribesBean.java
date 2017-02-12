ec80ac0fd31db49672f8265e5eff80d998350dde
Liz McGurty <emcgurty2@gmail.com>
2017-02-12 10:52:14 -0500

    using CDI

--- src/java/echomarket/web/managedbeans/ContactDescribesBean.java
+++ src/java/echomarket/web/managedbeans/ContactDescribesBean.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package echomarket.web.managedbeans;
 
 import echomarket.hibernate.ContactDescribes;
@@ -11,17 +6,16 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
-import javax.faces.bean.ManagedBean;
 import javax.inject.Named;
 import javax.persistence.Id;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 @Named
-@ManagedBean
 @RequestScoped
 public class ContactDescribesBean extends AbstractBean implements Serializable {
 
+  private static final long serialVersionUID = 1L;
   private Integer contact_describe_id;
   private Integer borrowerOrLender;
   private Integer optionValue;
@@ -60,7 +54,7 @@ public class ContactDescribesBean extends AbstractBean implements Serializable {
     } else {
       returnCD = "Contact Description not found";
     }
-     
+
     result = null;
     return returnCD;
   }
