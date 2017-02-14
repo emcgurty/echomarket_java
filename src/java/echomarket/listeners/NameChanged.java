1acd183957e6ef93db46d8e854f410ae24f66a49
Liz McGurty <emcgurty2@gmail.com>
2017-02-14 05:47:52 -0500

    clean up

--- src/java/echomarket/listeners/NameChanged.java
+++ src/java/echomarket/listeners/NameChanged.java
@@ -7,29 +7,30 @@ import javax.faces.event.AbortProcessingException;
 import javax.faces.event.ValueChangeEvent;
 import javax.faces.event.ValueChangeListener;
 
-
 /**
- * <p>Value change listener for the name entered on the
+ * <p>
+ * Value change listener for the name entered on the
  * <code>bookcashier.xhtml</code> page.</p>
  */
 public class NameChanged extends Object implements ValueChangeListener {
-    private static final Logger logger = Logger.getLogger(
-                "echo_market.listeners.NameChanged");
 
-    @Override
-    public void processValueChange(ValueChangeEvent event)
-        throws AbortProcessingException {
-        logger.log(Level.INFO, "Entering NameChanged.processValueChange");
+  private static final Logger logger = Logger.getLogger(
+          "echo_market.listeners.NameChanged");
 
-        String current = event.getComponent().getId();
-        logger.log(Level.INFO, "current in NameChanges is {0}", current);
-        
-        
-        if (null != event.getNewValue()) {
-             if ("name".equals(current)) {
-                       FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put( "name", event.getNewValue()); }
-             else if ("selected_zip_code".equals(current)) {
-            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put( "selected_zip_code", event.getNewValue()); }
-        }
+  @Override
+  public void processValueChange(ValueChangeEvent event)
+          throws AbortProcessingException {
+    logger.log(Level.INFO, "Entering NameChanged.processValueChange");
+
+    String current = event.getComponent().getId();
+    logger.log(Level.INFO, "current in NameChanges is {0}", current);
+
+    if (null != event.getNewValue()) {
+      if ("name".equals(current)) {
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("name", event.getNewValue());
+      } else if ("selected_zip_code".equals(current)) {
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("selected_zip_code", event.getNewValue());
+      }
     }
+  }
 }
